c6cb8c3d4303e040852c6399fa4209ec1ea51bba
Denis Tsibaev <denis.tsibaev@gmail.com>
2021-03-05 20:28:47 +0700

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -291,7 +291,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60676" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65393" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
